e29b3fe5cf9965a1e696322d3b4bfa58b790f3a8
stevecancode <scarrillo57@gmail.com>
2024-05-23 00:44:34 -0400

    message

--- health_analysis.js
+++ health_analysis.js
@@ -16,14 +16,47 @@ function addPatient() {
     }
   }
 
-  function resetForm() {
+function resetForm() {
     document.getElementById("name").value = "";
     document.querySelector('input[name="gender"]:checked').checked = false;
     document.getElementById("age").value = "";
     document.getElementById("condition").value = "";
   }
 
-  function generateReport() {
+  function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+
+btnSearch.addEventListener('click', searchCondition);
+
+function generateReport() {
     const numPatients = patients.length;
     const conditionsCount = {
       Diabetes: 0,
@@ -31,12 +64,12 @@ function addPatient() {
       "High Blood Pressure": 0,
     };
     const genderConditionsCount = {
-      Male: {
+        Male: {
         Diabetes: 0,
         Thyroid: 0,
         "High Blood Pressure": 0,
       },
-      Female: {
+        Female: {
         Diabetes: 0,
         Thyroid: 0,
         "High Blood Pressure": 0,
@@ -50,6 +83,7 @@ function addPatient() {
 
     report.innerHTML = `Number of patients: ${numPatients}<br><br>`;
     report.innerHTML += `Conditions Breakdown:<br>`;
+
     for (const condition in conditionsCount) {
       report.innerHTML += `${condition}: ${conditionsCount[condition]}<br>`;
     }
